36adbfdb4cf4778b6314111c32bdf50ac6deaa41
Jeffery <lidarkfire@gmail.com>
2017-05-23 12:12:49 -0400

    Changes comments Finished code

--- Dialectizer/V2/L33tspk.py
+++ Dialectizer/V2/L33tspk.py
@@ -1,7 +1,6 @@
 # -------------------------------------------------------------------------------
 # Name:	    1337.py
-# Purpose:  Convert add a Chinese dialect to an inputted text file(source.txt)
-#           and output the dialectized file as output.txt
+# Purpose:  Converts source.txt into a 1337 dialect, and outputs it to translated.txt
 #
 #
 # Author:	Me
@@ -25,7 +24,7 @@ def convert_letters(text_list, index, input, output):
 
     # Check if the current index matches the input
     for i in range(input_length):
-        if text_list[index + i] != input[i]:
+        if text_list[index+i] != input[i]:
             return
 
     # Since we didn't return in the previous function
@@ -36,28 +35,47 @@ def convert_letters(text_list, index, input, output):
     for i in range(output_length):
         text_list.insert(index + i, output[i])
 
+# Open source.txt, read it and define the text variable with that variable
 source = open("source.txt", "r")
-
 text = source.read()
 
-text_list = list(text)
-
-alphabet_original   = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-alphabet_translated = ["@","8","c","d","3","f","g","h","i","J","k","1","m","n","0","p","9","r","s","7","u","v","w","x","y","z"]
-
-index = 0
-while index <= len(text_list) - 1:
-    convert_letters(text_list, index, ". ", ". I'm gonna DDOS u!!!!")
-
-    for i in range(len(alphabet_original)):
-        convert_letters(text_list, index, alphabet_original[i], alphabet_translated[i])
-
-    index += 1
-
+# Close source.txt
 source.close()
 
+# Convert text into a list of individual characters
+text_list = list(text)
+
+# Define a letter bank that has letters and their conversions
+alphabet_input  = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+alphabet_output = ["@","13","c","d","3","f","6","h","i","J","k","1","m","n","0","p","9","r","5","7","u","v","w","x","y","2"]
+
+# Make a while loop
+index = 0
+
+# The while loop iterates through the text as the index increases, until it is greater than or equal to the length of the text
+while index <= len(text_list) - 1:
+
+    # Convert input letters in the word bank to their correspinding outputs
+    for i in range(len(alphabet_input)):
+        convert_letters(text_list, index, alphabet_input[i], alphabet_output[i])
+
+    # Convert input words into their corresponding translations
+    convert_letters(text_list, index, "hacks", "hax")
+    convert_letters(text_list, index, "sucks", "sux")
+    convert_letters(text_list, index, "owned", "pwned")
+    convert_letters(text_list, index, "banned", "b&")
+
+    # Convert input phrases into their corresponding translations
+    convert_letters(text_list, index, ". ", ". I'm gunna DDOS u n00b!!!!")
+
+    # Increment index
+    index += 1
+
+
+# Give the text the new value of the new formatted list of letters
 text = "".join(text_list)
 
+# Open the translated file and write text to that file
 translated = open("translated.txt", "w")
 translated.write(text)
 
